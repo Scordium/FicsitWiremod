2705554d27d68671690ad5b57a448f65cf5990d8
Sam Crosscat <moboman8@gmail.com>
2023-05-30 23:19:30 +0300

    Add Lerp Color and Lerp Color Array

---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Color/LerpColor.h
@@ -0,0 +1,68 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/MultistateWiremodBuildable.h"
+#include "LerpColor.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API ALerpColor : public AMultistateWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Process_Implementation(float DeltaTime) override
+	{
+		//Default mode - lerp from A to B
+		if(CurrentStateIndex == 0)
+		{
+			auto A = WM_GetColor(0);
+			auto B = WM_GetColor(1);
+			auto Alpha = WM_GetFloat(2);
+
+			Out = UKismetMathLibrary::LinearColorLerp(A, B, Alpha);
+		}
+		//Alt mode - lerp in color array
+		else if (CurrentStateIndex == 1)
+		{
+			auto Colors = WM::GetColorArray(GetConnection(0));
+			auto NumColors = Colors.Num();
+			
+			if(NumColors == 0)
+			{
+				Out = FLinearColor::Black;
+			}
+			else if(NumColors == 1)
+			{
+				Out = Colors[0];
+			}
+			else
+			{
+				auto Alpha = WM_GetFloat(1);
+				auto AlphaPerIndex = 1.f / Colors.Num();
+
+				//This is so fucking stupid
+				auto AIndex = FMath::Clamp((int)(Alpha / AlphaPerIndex - 1), 0, NumColors - 2);
+				auto BIndex = FMath::Clamp((int)(Alpha / AlphaPerIndex + 1), 1, NumColors - 1);
+				
+				auto A = Colors[AIndex];
+				auto B = Colors[BIndex];
+
+				Out = UKismetMathLibrary::LinearColorLerp(A, B, Alpha);
+			}
+			
+			
+		}
+	}
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(ALerpColor, Out)
+	}
+
+	UPROPERTY(Replicated)
+	FLinearColor Out;
+};
