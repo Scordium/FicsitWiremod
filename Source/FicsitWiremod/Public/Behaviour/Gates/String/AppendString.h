3fa11f59a157c123e33357b8fd4ebe8164f9f7c5
Sam Crosscat <moboman8@gmail.com>
2023-06-09 19:47:10 +0300

    Convert old gates into gate modes

---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/String/AppendString.h
@@ -0,0 +1,47 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/MultistateWiremodBuildable.h"
+#include "AppendString.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API AAppendString : public AMultistateWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Process_Implementation(float DeltaTime) override
+	{
+		//Default mode - multiple inputs for values and one input for separator
+		if(CurrentStateIndex == 0)
+		{
+			TArray<FString> Values;
+			FString Separator = GetConnection(0).GetString();
+
+			for(int i = 1; i < InputConnections.Num(); i++)
+				Values.Add(GetConnection(i).GetString());
+
+			Out = FString::Join(Values, *Separator);
+		}
+		//Alt mode - one input for array, one input for separator
+		else if(CurrentStateIndex == 1)
+		{
+			TArray<FString> Values = GetConnection(0).GetStringArray();
+			FString Separator = GetConnection(1).GetString();
+
+			Out = FString::Join(Values, *Separator);
+		}
+	}
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(AAppendString, Out)
+	}
+
+	UPROPERTY(Replicated, SaveGame)
+	FString Out;
+};
---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/String/StringCase.h
@@ -0,0 +1,53 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/MultistateWiremodBuildable.h"
+#include "StringCase.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API AStringCase : public AMultistateWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Process_Implementation(float DeltaTime) override
+	{
+		//To lowercase
+		if(CurrentStateIndex == 0) Out = GetConnection(0).GetString().ToLower();
+		//To UPPERCASE
+		else if(CurrentStateIndex == 1) Out = GetConnection(0).GetString().ToUpper();
+		//To Titlecase
+		else if(CurrentStateIndex == 2)
+		{
+			Out = GetConnection(0).GetString();
+			if(Out.Len() == 0) return;
+			
+			//What
+			Out[0] = FChar::ToUpper(Out[0]);
+		}
+		//To SaRcAsMcAsE
+		else if(CurrentStateIndex == 3)
+		{
+			Out = "";
+			auto Source = GetConnection(0).GetString();
+
+			for(int i = 0; i < Source.Len(); i++)
+			{
+				Out += i%2 ? FChar::ToLower(Source[0]) : FChar::ToUpper(Source[0]);;
+			}
+		}
+	}
+
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(AStringCase, Out)
+	}
+
+	UPROPERTY(Replicated, SaveGame)
+	FString Out;
+};
