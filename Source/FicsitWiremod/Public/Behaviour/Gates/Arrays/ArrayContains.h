a399209160ee029655ff2d77e029f30837ec7a6e
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2023-06-30 10:54:47 +0300

    Implement Array Contains gate

---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Arrays/ArrayContains.h
@@ -0,0 +1,41 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/FGWiremodBuildable.h"
+#include "CommonLib/DynamicValues/CCArrayValueBase.h"
+#include "CommonLib/DynamicValues/CCDynamicValueUtils.h"
+#include "ArrayContains.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API AArrayContains : public AFGWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Process_Implementation(float DeltaTime) override
+	{
+		auto Connection = GetConnection(0);
+		ArrayCache = UCCDynamicValueUtils::FromValue(Connection, ArrayCache ? (UObject*) ArrayCache : (UObject*) this);
+
+		if(auto Array = Cast<UCCArrayValueBase>(ArrayCache))
+			Out = Array->Contains(GetConnection(1));
+		else Out = false;
+
+		SetInputType(1, UConnectionTypeFunctions::ArrayToBase(Connection.ConnectionType));
+	}
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(AArrayContains, Out)
+	}
+
+	UPROPERTY()
+	UCCDynamicValueBase* ArrayCache;
+
+	UPROPERTY(Replicated, SaveGame)
+	bool Out;
+};
