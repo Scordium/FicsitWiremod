b63fc8851a3523ffecda1fefaf527ea00120b4f1
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2023-08-02 09:29:42 +0300

    Universal change detector

---
+++ Source/FicsitWiremod/Public/Behaviour/Miscellaneous/ChangeDetector.h
@@ -0,0 +1,39 @@
+// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/FGWiremodBuildable.h"
+#include "CommonLib/DynamicValues/CCDynamicValueUtils.h"
+#include "ChangeDetector.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API AChangeDetector : public AFGWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ServerProcess_Implementation(double DeltaTime) override
+	{
+		auto Value = UCCDynamicValueUtils::FromValue(GetConnection(0), this);
+
+		if(CachedValue == nullptr) Out = false;
+		else Out = !CachedValue->Equals(Value);
+
+		CachedValue = Value;
+	}
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(AChangeDetector, Out)
+	}
+
+
+	UPROPERTY(Replicated, SaveGame)
+	bool Out;
+
+	UPROPERTY(SaveGame)
+	UCCDynamicValueBase* CachedValue;
+};
