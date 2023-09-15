0eb985d2ee5c323108403259add7386ced8dc127
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2023-09-15 21:52:03 +0300

    Vector grid snap gate

---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Vector/VectorGridSnap.h
@@ -0,0 +1,32 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/FGWiremodBuildable.h"
+#include "VectorGridSnap.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API AVectorGridSnap : public AFGWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ServerProcess_Implementation(double DeltaTime) override
+	{
+		auto Value = GetConnection(0).GetVector();
+		auto GridSize = GetConnection(1).GetFloat();
+
+		Out = Value.GridSnap(GridSize);
+	}
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(AVectorGridSnap, Out)
+	}
+
+	UPROPERTY(SaveGame, Replicated)
+	FVector Out;
+};
