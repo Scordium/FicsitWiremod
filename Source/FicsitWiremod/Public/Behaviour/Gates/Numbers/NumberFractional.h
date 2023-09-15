f6b3299f667ec3780020b5ee4f5db045496a462d
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2023-09-15 21:51:47 +0300

    Number fraction gate

---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Numbers/NumberFractional.h
@@ -0,0 +1,29 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/FGWiremodBuildable.h"
+#include "NumberFractional.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API ANumberFractional : public AFGWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ServerProcess_Implementation(double DeltaTime) override
+	{
+		Out = FMath::Fractional(GetConnection(0).GetFloat());
+	}
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(ANumberFractional, Out)
+	}
+
+	UPROPERTY(SaveGame, Replicated)
+	double Out;
+};
