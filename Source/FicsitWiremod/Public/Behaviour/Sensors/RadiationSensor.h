3a4c941a16c487c89276d0f07ca77a9b084e9616
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2023-12-28 15:27:35 +0300

    Radiation sensor

---
+++ Source/FicsitWiremod/Public/Behaviour/Sensors/RadiationSensor.h
@@ -0,0 +1,30 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/FGWiremodBuildable.h"
+#include "RadiationSensor.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API ARadiationSensor : public AFGWiremodBuildable, public IFGRadiationInterface
+{
+	GENERATED_BODY()
+
+	virtual void ReceiveRadiation_Implementation(float amount, float duration, FVector direction, TSubclassOf<UFGDamageType> damageType) override
+	{
+		RadiationAmount = amount;
+	}
+
+	virtual bool CanReceiveRadiation_Implementation() const override { return true; }
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(ARadiationSensor, RadiationAmount)
+	}
+	
+	UPROPERTY(Replicated)
+	double RadiationAmount;
+};
