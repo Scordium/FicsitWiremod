250bd576a713692bb6c36fa656599886d697ace0
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2024-03-29 22:59:27 +0300

    Array mode for Min and Max gates

--- Source/FicsitWiremod/Public/Behaviour/Gates/Numbers/NumberMax.h
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Numbers/NumberMax.h
@@ -4,17 +4,21 @@
 
 #include "CoreMinimal.h"
 #include "Behaviour/FGWiremodBuildable.h"
+#include "Behaviour/MultistateWiremodBuildable.h"
 #include "NumberMax.generated.h"
 
 UCLASS()
-class FICSITWIREMOD_API ANumberMax : public AFGWiremodBuildable
+class FICSITWIREMOD_API ANumberMax : public AMultistateWiremodBuildable
 {
 	GENERATED_BODY()
 
 public:
 	virtual void ServerProcess_Implementation(double DeltaTime) override
 	{
-		Out = FMath::Max(GetConnection(0).GetFloat(), GetConnection(1).GetFloat());
+		//0 - Default mode, pick one out of two numbers
+		if(CurrentStateIndex == 0) Out = FMath::Max(GetConnection(0).GetFloat(), GetConnection(1).GetFloat());
+		//1 - Array mode, pick one value in array
+		else if (CurrentStateIndex == 1) Out = FMath::Max(GetConnection(0).GetFloatArray());
 	}
 
 	virtual void GetLifetimeReplicatedProps( TArray<FLifetimeProperty>& OutLifetimeProps ) const override
--- Source/FicsitWiremod/Public/Behaviour/Gates/Numbers/NumberMin.h
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Numbers/NumberMin.h
@@ -4,17 +4,21 @@
 
 #include "CoreMinimal.h"
 #include "Behaviour/FGWiremodBuildable.h"
+#include "Behaviour/MultistateWiremodBuildable.h"
 #include "NumberMin.generated.h"
 
 UCLASS()
-class FICSITWIREMOD_API ANumberMin : public AFGWiremodBuildable
+class FICSITWIREMOD_API ANumberMin : public AMultistateWiremodBuildable
 {
 	GENERATED_BODY()
 
 public:
 	virtual void ServerProcess_Implementation(double DeltaTime) override
 	{
-		Out = FMath::Min(GetConnection(0).GetFloat(), GetConnection(1).GetFloat());
+		//0 - Default mode, pick one out of two numbers
+		if(CurrentStateIndex == 0) Out = FMath::Min(GetConnection(0).GetFloat(), GetConnection(1).GetFloat());
+		//1 - Array mode, pick one value in array
+		else if (CurrentStateIndex == 1) Out = FMath::Min(GetConnection(0).GetFloatArray());
 	}
 
 	virtual void GetLifetimeReplicatedProps( TArray<FLifetimeProperty>& OutLifetimeProps ) const override
