fd51eaa08bf86e2229f4c764c3e03513d4d918f4
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2024-04-07 20:31:27 +0300

    Combine stacks gate

---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Arrays/CombineItemStacks.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/FGWiremodBuildable.h"
+#include "CommonLib/InventoryUtilities.h"
+#include "CombineItemStacks.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API ACombineItemStacks : public AFGWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ServerProcess_Implementation(double DeltaTime) override
+	{
+		Out = UInventoryUtilities::CombineStacks(GetConnection(0).GetStackArray());
+	}
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(ACombineItemStacks, Out)
+	}
+
+	UPROPERTY(Replicated, SaveGame)
+	TArray<FInventoryStack> Out;
+};
