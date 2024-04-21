e2d24fc65b701d443233e44b458c0b2c9b7c76f1
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2024-04-22 00:12:17 +0300

    Get item stack by descriptor gate

---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Arrays/GetItemStack.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Behaviour/FGWiremodBuildable.h"
+#include "GetItemStack.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API AGetItemStack : public AFGWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ServerProcess_Implementation(double DeltaTime) override
+	{
+		auto Stacks = GetConnection(0).GetStackArray();
+		auto Filter = GetConnection(1).GetItemDescriptor();
+		
+		//Find stack that matches filter class and set it as output
+		for(auto Stack : Stacks)
+		{
+			if(Stack.Item.GetItemClass()->IsChildOf(Filter))
+			{
+				Out = Stack;
+				return;
+			}
+		}
+
+		//If nothing was found, use default values
+		Out.Item = FInventoryItem::NullInventoryItem;
+		Out.NumItems = 0;
+	}
+
+	UFUNCTION() TSubclassOf<UFGItemDescriptor> GetDescriptor() const { return Out.Item.GetItemClass(); }
+	UFUNCTION() int GetNumItems() const { return Out.NumItems; }
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(AGetItemStack, Out)
+	}
+
+	UPROPERTY(Replicated, SaveGame)
+	FInventoryStack Out;
+};
