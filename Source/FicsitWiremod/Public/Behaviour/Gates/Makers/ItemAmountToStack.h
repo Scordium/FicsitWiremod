f2e71962920fb93ba96fbe7a8a963705e79c0a5e
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2024-04-29 00:48:31 +0300

    Array mode for item struct converters

--- Source/FicsitWiremod/Public/Behaviour/Gates/Makers/ItemAmountToStack.h
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Makers/ItemAmountToStack.h
@@ -4,20 +4,45 @@
 
 #include "CoreMinimal.h"
 #include "Behaviour/FGWiremodBuildable.h"
+#include "Behaviour/MultistateWiremodBuildable.h"
 #include "ItemAmountToStack.generated.h"
 
 UCLASS()
-class FICSITWIREMOD_API AItemAmountToStack : public AFGWiremodBuildable
+class FICSITWIREMOD_API AItemAmountToStack : public AMultistateWiremodBuildable
 {
 	GENERATED_BODY()
 
 public:
 	virtual void ServerProcess_Implementation(double DeltaTime) override
 	{
-		auto ItemAmount = GetConnection(0).GetItemAmount();
+		//0 - Default mode, single struct
+		if(CurrentStateIndex == 0)
+		{
+			auto ItemAmount = GetConnection(0).GetItemAmount();
 
-		Out.Item.SetItemClass(ItemAmount.ItemClass);
-		Out.NumItems = ItemAmount.Amount;
+			Out.Item.SetItemClass(ItemAmount.ItemClass);
+			Out.NumItems = ItemAmount.Amount;
+		}
+		//1 - Alt mode, struct array
+		else if(CurrentStateIndex == 1)
+		{
+			auto ItemAmounts = GetConnection(0).GetItemAmountArray();
+			OutArray.SetNum(ItemAmounts.Num());
+
+			for(int i = 0; i < ItemAmounts.Num(); i++)
+			{
+				OutArray[i].Item.SetItemClass(ItemAmounts[i].ItemClass);
+				OutArray[i].NumItems = ItemAmounts[i].Amount;
+			}
+		}
+	}
+
+	virtual void OnStateSelected_Internal(int Index) override
+	{
+		Super::OnStateSelected_Internal(Index);
+
+		if(Index == 0) OutArray.Empty();
+		else if (Index == 1) Out = FInventoryStack();
 	}
 
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
@@ -25,8 +50,12 @@ public:
 		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 		DOREPLIFETIME(AItemAmountToStack, Out)
+		DOREPLIFETIME(AItemAmountToStack, OutArray)
 	}
 
 	UPROPERTY(Replicated, SaveGame)
 	FInventoryStack Out;
+
+	UPROPERTY(Replicated, SaveGame)
+	TArray<FInventoryStack> OutArray;
 };
--- Source/FicsitWiremod/Public/Behaviour/Gates/Makers/ItemStackToAmount.h
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Makers/ItemStackToAmount.h
@@ -4,20 +4,45 @@
 
 #include "CoreMinimal.h"
 #include "Behaviour/FGWiremodBuildable.h"
+#include "Behaviour/MultistateWiremodBuildable.h"
 #include "ItemStackToAmount.generated.h"
 
 UCLASS()
-class FICSITWIREMOD_API AItemStackToAmount : public AFGWiremodBuildable
+class FICSITWIREMOD_API AItemStackToAmount : public AMultistateWiremodBuildable
 {
 	GENERATED_BODY()
 
 public:
 	virtual void ServerProcess_Implementation(double DeltaTime) override
 	{
-		const auto Stack = GetConnection(0).GetStack();
+		//0 - Default mode, single struct
+		if(CurrentStateIndex == 0)
+		{
+			auto ItemStack = GetConnection(0).GetStack();
 
-		Out.ItemClass = Stack.Item.GetItemClass();
-		Out.Amount = Stack.NumItems;
+			Out.ItemClass = ItemStack.Item.GetItemClass();
+			Out.Amount = ItemStack.NumItems;
+		}
+		//1 - Alt mode, struct array
+		else if(CurrentStateIndex == 1)
+		{
+			auto ItemStacks = GetConnection(0).GetStackArray();
+			OutArray.SetNum(ItemStacks.Num());
+
+			for(int i = 0; i < ItemStacks.Num(); i++)
+			{
+				OutArray[i].ItemClass = ItemStacks[i].Item.GetItemClass();
+				OutArray[i].Amount = ItemStacks[i].NumItems;
+			}
+		}
+	}
+
+	virtual void OnStateSelected_Internal(int Index) override
+	{
+		Super::OnStateSelected_Internal(Index);
+
+		if(Index == 0) OutArray.Empty();
+		else if (Index == 1) Out = FItemAmount();
 	}
 
 
@@ -26,8 +51,12 @@ public:
 		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 
 		DOREPLIFETIME(AItemStackToAmount, Out)
+		DOREPLIFETIME(AItemStackToAmount, OutArray)
 	}
 
 	UPROPERTY(Replicated, SaveGame)
 	FItemAmount Out;
+
+	UPROPERTY(Replicated, SaveGame)
+	TArray<FItemAmount> OutArray;
 };
