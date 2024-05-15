3f0573e425551d7396937faf4d44b03754ba7156
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2024-05-15 16:19:03 +0300

    Array mode for subtract gate

--- Source/FicsitWiremod/Public/Behaviour/Gates/Numbers/NumberSubtract.h
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Numbers/NumberSubtract.h
@@ -4,17 +4,36 @@
 
 #include "CoreMinimal.h"
 #include "Behaviour/FGWiremodBuildable.h"
+#include "Behaviour/MultistateWiremodBuildable.h"
 #include "NumberSubtract.generated.h"
 
 UCLASS()
-class FICSITWIREMOD_API ANumberSubtract : public AFGWiremodBuildable
+class FICSITWIREMOD_API ANumberSubtract : public AMultistateWiremodBuildable
 {
 	GENERATED_BODY()
 
 public:
 	virtual void ServerProcess_Implementation(double DeltaTime) override
 	{
-		Out = GetConnection(0).GetFloat() - GetConnection(1).GetFloat();
+		//Default mode subtract B from A
+		if(CurrentStateIndex == 0) Out = GetConnection(0).GetFloat() - GetConnection(1).GetFloat();
+		//Alt mode - subtract all subsequent elements from the first element
+		else if(CurrentStateIndex == 1)
+		{
+			const auto Array = GetConnection(0).GetFloatArray();
+			if(Array.Num() == 0) Out = 0;
+			else if(Array.Num() == 1) Out = Array[0];
+			else
+			{
+				double ArrOut = Array[0];
+				for (int i = 1; i < Array.Num(); i++)
+				{
+					ArrOut -= Array[i];
+				}
+
+				Out = ArrOut;
+			}
+		}
 	}
 
 	virtual void GetLifetimeReplicatedProps( TArray<FLifetimeProperty>& OutLifetimeProps ) const override
