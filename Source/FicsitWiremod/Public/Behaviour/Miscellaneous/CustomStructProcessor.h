ca92a5f2aee9149605116ca65af3fd0720e7d39f
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2024-05-25 19:23:34 +0300

    Change struct recompiling logic for better UX

--- Source/FicsitWiremod/Public/Behaviour/Miscellaneous/CustomStructProcessor.h
+++ Source/FicsitWiremod/Public/Behaviour/Miscellaneous/CustomStructProcessor.h
@@ -73,8 +73,27 @@ public:
 
 	void SetCustomStruct_Internal(const FCustomStruct& NewStruct)
 	{
-		//Disconnect all inputs
-		OnInputDisconnected_Internal(-1);
+		//Disconnect all changed inputs
+		for(int i = 0; i < NewStruct.Values.Num(); i++)
+		{
+			if(i >= Out.Values.Num()) break;
+
+			auto& OldValue = Out.Values[i];
+			auto& NewValue = NewStruct.Values[i];
+			
+			if(!NewValue.Value || OldValue.Value->ConnectionType != NewValue.Value->ConnectionType || OldValue.Name != NewValue.Name)
+				OnInputDisconnected_Internal(i);
+		}
+
+		//Disconnect all stale inputs that remain in case the new struct is smaller.
+		if(Out.Values.Num() > NewStruct.Values.Num())
+		{
+			for(int i = Out.Values.Num() - 1; i >= NewStruct.Values.Num(); i--)
+			{
+				OnInputDisconnected_Internal(i);
+			}
+		}
+		
 		Out = NewStruct;
 		Recompile();
 	}
