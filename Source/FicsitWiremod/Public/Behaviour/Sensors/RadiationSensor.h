82162e198026fa8d523b3b536abcbf56aab53644
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2024-10-27 19:28:30 +0300

    Fix radiation sensor

--- Source/FicsitWiremod/Public/Behaviour/Sensors/RadiationSensor.h
+++ Source/FicsitWiremod/Public/Behaviour/Sensors/RadiationSensor.h
@@ -4,18 +4,32 @@
 
 #include "CoreMinimal.h"
 #include "FGRadiationInterface.h"
+#include "FGRadioactivitySubsystem.h"
 #include "FGWiremodBuildable.h"
-#include "Runtime/CoreUObject/Public/Templates/SubclassOf.h"
 #include "RadiationSensor.generated.h"
 
 UCLASS()
 class FICSITWIREMOD_API ARadiationSensor : public AFGWiremodBuildable, public IFGRadiationInterface
 {
 	GENERATED_BODY()
+
+	virtual void BeginPlay() override
+	{
+		Super::BeginPlay();
+
+		if (HasAuthority())
+		{
+			if (auto GameState = Cast<AFGGameState>(UGameplayStatics::GetGameState(this)))
+			{
+				auto RadiationSystem = GameState->GetRadioactivitySubsystem();
+				if (RadiationSystem) RadiationSystem->mPotentiallyAffectedActors.AddUnique(this);
+			}
+		} 
+	}
 	
 	virtual void ReceiveRadiation_Implementation(float amount, float duration, FVector direction, TSubclassOf<UFGDamageType> damageType) override
 	{
-		RadiationAmount = amount;
+		RadiationAmount = amount > 0.0047 ? amount : 0;
 	}
 
 	virtual bool CanReceiveRadiation_Implementation() const override { return true; }
