ba25fa1aeca7d1bd08aab138fd2f9d51cddb26f5
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2024-12-15 22:49:03 +0300

    Fix converter crash due to iterating all actors

---
+++ Source/FicsitWiremod/Private/Utility/CircuitryDeprecatorUtils.cpp
@@ -0,0 +1,1 @@
+﻿#include "CircuitryDeprecatorUtils.h"
--- Source/FicsitWiremod/Public/Utility/CircuitryDeprecatorUtils.h
+++ Source/FicsitWiremod/Public/Utility/CircuitryDeprecatorUtils.h
@@ -23,36 +23,30 @@ public:
 	UFUNCTION(BlueprintCallable, meta=(DefaultToSelf="Old"))
 	static void GenerateMultistateBuildable(AFGWiremodBuildable* Old, TSubclassOf<AMultistateWiremodBuildable> Class, int StateIndex, TSubclassOf<UFGRecipe> WithRecipe)
 	{
+		Old->DestroyWires();
 		auto const Transform = Old->GetActorTransform();
 		auto NewBuildable = Cast<AMultistateWiremodBuildable>(Old->GetWorld()->SpawnActor(Class.GetDefaultObject()->GetClass(), &Transform));
 		NewBuildable->SetBuiltWithRecipe(WithRecipe);
-		
-		//Calling this *before* setting owner data to avoid problems with object ownership getting in the way
-		NewBuildable->OnStateSelected(StateIndex, NULL); //Set the state index
+
+		NewBuildable->OnStateSelected_Internal(StateIndex); //Set the state index
 		NewBuildable->InputConnections = Old->InputConnections; //Transfer connections
-		
 		NewBuildable->OwnerData = Old->OwnerData; // Transfer owner data
-
-		TArray<AActor*> Gates;
-		UGameplayStatics::GetAllActorsOfClass(Old, AFGWiremodBuildable::StaticClass(), Gates);
-
-		for(auto Actor : Gates)
+		
+		TArray<AFGWiremodBuildable*> Gates;
+		AFGBuildableSubsystem::Get(Old)->GetTypedBuildable<AFGWiremodBuildable>(Gates);
+		
+		for(auto Gate : Gates)
 		{
-			auto Gate = Cast<AFGWiremodBuildable>(Actor);
 			if(Gate->InputConnections.Num() == 0) continue;
-			for(auto& Input : Gate->InputConnections)
-			{
-				if(Input.Object == Old)
-					Input.Object = NewBuildable;
-			}
 
+			for(auto& Input : Gate->InputConnections)
+				if(Input.Object == Old) Input.Object = NewBuildable;
+			
 			Gate->DrawWires_Implementation();
 		}
 
 		ACircuitryLogger::DispatchWarningEvent(Old->GetName() + " was converted into " + NewBuildable->GetName());
 		// "Do not directly call Event functions in Interfaces" my ass, UE.
 		IFGDismantleInterface::Execute_Dismantle(Old);
-		
-		NewBuildable->DrawWires_Implementation(); //Redraw wires because they're not visible after transferring connections.
 	}
 };
