db4bea18583ebaf7cbbafde9afa4d98135a5a34a
Sam Crosscat <66058973+Scordium@users.noreply.github.com>
2025-06-10 08:59:07 +0300

    Splitter rule and item descriptor info modules

---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Breakers/BreakItemDescriptor.h
@@ -0,0 +1,103 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FGWiremodBuildable.h"
+#include "Runtime/CoreUObject/Public/Templates/SubclassOf.h"
+
+#include "BreakItemDescriptor.generated.h"
+
+#define GET_CACHE_CHECKED(ret) if(DescriptorCache == nullptr) return ret;\
+	auto CDO = DescriptorCache.GetDefaultObject();
+
+UCLASS()
+class FICSITWIREMOD_API ABreakItemDescriptor : public AFGWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ServerProcess_Implementation(double DeltaTime) override
+	{
+		DescriptorCache = GetConnection(0).GetItemDescriptor();
+	}
+
+	
+	UFUNCTION()
+	FString GetItemName()
+	{
+		GET_CACHE_CHECKED("");
+		return CDO->mDisplayName.ToString();
+	}
+
+	UFUNCTION()
+	FString GetItemDesc()
+	{
+		GET_CACHE_CHECKED("");
+		return CDO->mDescription.ToString();
+	}
+
+	UFUNCTION()
+	double GetItemAmountSize()
+	{
+		GET_CACHE_CHECKED(-1);
+
+		return UFGItemDescriptor::GetStackSize(DescriptorCache);
+	}
+
+	UFUNCTION()
+	bool GetCanDiscard()
+	{
+		return UFGItemDescriptor::CanBeDiscarded(DescriptorCache);
+	}
+
+	UFUNCTION()
+	int GetItemType()
+	{
+		GET_CACHE_CHECKED(-1);
+		
+		return (int) CDO->Form();
+	}
+
+	UFUNCTION()
+	bool GetIsSolid()
+	{
+		GET_CACHE_CHECKED(false);
+
+		return CDO->Form() == EResourceForm::RF_SOLID;
+	}
+
+	UFUNCTION()
+	bool GetIsLiquid()
+	{
+		GET_CACHE_CHECKED(false);
+
+		return CDO->Form() == EResourceForm::RF_LIQUID;
+	}
+
+	UFUNCTION()
+	bool GetIsGas()
+	{
+		GET_CACHE_CHECKED(false);
+
+		return CDO->Form() == EResourceForm::RF_GAS;
+	}
+
+	UFUNCTION()
+	UTexture* GetItemIcon()
+	{
+		GET_CACHE_CHECKED(nullptr);
+
+		return UFGItemDescriptor::GetBigIcon(DescriptorCache);
+	}
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+		DOREPLIFETIME(ABreakItemDescriptor, DescriptorCache);
+	}
+
+	UPROPERTY(Replicated, SaveGame)
+	TSubclassOf<UFGItemDescriptor> DescriptorCache;
+};
---
+++ Source/FicsitWiremod/Public/Behaviour/Gates/Breakers/BreakSplitterRule.h
@@ -0,0 +1,35 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "FGWiremodBuildable.h"
+#include "BreakSplitterRule.generated.h"
+
+UCLASS()
+class FICSITWIREMOD_API ABreakSplitterRule : public AFGWiremodBuildable
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ServerProcess_Implementation(double DeltaTime) override
+	{
+		ValueCache = GetConnection(0).GetSplitterRule();
+	}
+
+	UFUNCTION()
+	TSubclassOf<UFGItemDescriptor> GetDescriptor() { return ValueCache.ItemClass; }
+
+	UFUNCTION()
+	int GetOutputIndex() { return ValueCache.OutputIndex; }
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override
+	{
+		Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+		DOREPLIFETIME(ABreakSplitterRule, ValueCache);
+	}
+
+
+	UPROPERTY(SaveGame, Replicated)
+	FSplitterSortRule ValueCache;
+};
